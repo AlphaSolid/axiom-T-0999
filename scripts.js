b7eedba1ceb3f2bb9509bed054c9528a4d06a684
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2022-06-16 21:59:42 +0800

    v1.0.0 but it doesn't make any sense

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+var front = '$user' + ' ';
+var inp = document.getElementById('userInput');
+var cons = document.getElementById('console');
 
+function processInput() {
+    print(inp.value);
+
+
+
+    inp.value = '';
+    print('<br><br>' + front);
+}
+
+let print = (val) => cons.innerHTML += val;
+
+print(front);
