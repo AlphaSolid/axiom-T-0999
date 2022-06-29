bcb09916b95f6c6cddecb62833c92d154fe3c2dd
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2022-06-29 21:16:28 +0800

    v0.1.1 added specific commands and fixed some bugs
    index.html is a must

--- scripts.js
+++ scripts.js
@@ -2,13 +2,28 @@ var front = '$user' + ' ';
 var inp = document.getElementById('userInput');
 var cons = document.getElementById('console');
 
+let cmdList = {
+    help: 'List of valid commands (this)',
+    ls: 'List of all existing files'
+};
+
 function processInput() {
-    print(inp.value);
+    print(inp.value + '<br>');
 
+    let cmd = inp.value.split(' ')[0];
+    if (cmd == 'help') {
+        for (const key of Object.keys(cmdList)) {
+            print(key + ': ' + cmdList[key] + '<br>');
+        };
+    } else if (cmd == 'ls') {
+        print('PLACEHOLDER 501');
+    } else {
+        print('No such command. Try \'help\' to list all valid commands.');
+    }
 
-
+    print('<br>');
+    print('<br>' + front);
     inp.value = '';
-    print('<br><br>' + front);
 }
 
 let print = (val) => cons.innerHTML += val;
