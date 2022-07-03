0b6b9d1d62203382571f90f0edafb6a665e534e5
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2022-07-03 21:58:27 +0800

    v0.2 fixed some bugs and added some functions
    lameCoding101

--- scripts.js
+++ scripts.js
@@ -1,22 +1,29 @@
-var front = '$user' + ' ';
+var front = 'user $' + ' ';
 var inp = document.getElementById('userInput');
 var cons = document.getElementById('console');
 
-let cmdList = {
+let cmdDict = {
     help: 'List of valid commands (this)',
-    ls: 'List of all existing files'
+    ls: 'List of all existing files',
+    open: 'Open an existing file'
 };
+
+let fileList = ['file.txt'];
 
 function processInput() {
     print(inp.value + '<br>');
 
-    let cmd = inp.value.split(' ')[0];
-    if (cmd == 'help') {
-        for (const key of Object.keys(cmdList)) {
-            print(key + ': ' + cmdList[key] + '<br>');
+    let cmd = inp.value.split(' ');
+    if (cmd[0] == 'help') {
+        for (const key of Object.keys(cmdDict)) {
+            print(key + ': ' + cmdDict[key] + '<br>');
         };
-    } else if (cmd == 'ls') {
-        print('PLACEHOLDER 501');
+    } else if (cmd[0] == 'ls') {
+        for (const files of fileList) {
+            print(files + '<br>');
+        };
+    } else if (cmd[0] == 'open') {
+        openFile(cmd[1]);
     } else {
         print('No such command. Try \'help\' to list all valid commands.');
     }
@@ -24,6 +31,14 @@ function processInput() {
     print('<br>');
     print('<br>' + front);
     inp.value = '';
+
+    function openFile(file) {
+        if (file == 'file.txt') {
+            print('PLACEHOLDER 503');
+        } else {
+            print('No such file. Please ensure that there are no additional characters in your command.');
+        }
+    }
 }
 
 let print = (val) => cons.innerHTML += val;
