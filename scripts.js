436acd3804bfff44cd3b538f0cdf972444b064f5
Nothing <106498416+AlphaSolid@users.noreply.github.com>
2022-07-06 21:36:29 +0800

    v1.0 lol
    Finally v1 yeet :D (what a lame update)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var front = 'user $' + ' ';
-var inp = document.getElementById('userInput');
-var cons = document.getElementById('console');
+const inp = document.getElementById('userInput');
+const cons = document.getElementById('console');
 
 let cmdDict = {
     help: 'List of valid commands (this)',
@@ -8,40 +8,48 @@ let cmdDict = {
     open: 'Open an existing file'
 };
 
-let fileList = ['file.txt'];
+let fileList = ['file.txt', 'count.exe'];
 
 function processInput() {
-    print(inp.value);
+    if (inp.value != '') {
+        print(inp.value);
 
-    let cmd = inp.value.split(' ');
-    if (cmd[0] == 'help') {
-        for (const key of Object.keys(cmdDict)) {
-            print(key + ': ' + cmdDict[key]);
-        };
-    } else if (cmd[0] == 'ls') {
-        for (const files of fileList) {
-            print(files);
-        };
-    } else if (cmd[0] == 'open') {
-        openFile(cmd[1]);
-    } else {
-        print('No such command. Try \'help\' to list all valid commands.');
-    }
-    
-    cons.innerHTML += '<br>' + front;
-    inp.value = '';
-
-    function openFile(file) {
-        if (fileList.includes(file)) {
-            print('————————————————————————————————————————————————');
-            if (file == 'file.txt') {
-                print('This is a placeholder text file.');
-            } else if (file == "") {
-                
-            }
-            print('————————————————————————————————————————————————');
+        let cmd = inp.value.split(' ');
+        if (cmd[0] == 'help') {
+            for (const key of Object.keys(cmdDict)) {
+                print(key + ': ' + cmdDict[key]);
+            };
+        } else if (cmd[0] == 'ls') {
+            for (const files of fileList) {
+                print(files);
+            };
+        } else if (cmd[0] == 'open') {
+            openFile(cmd[1]);
         } else {
-            print('No such file. Please ensure that there are no additional characters in your command.');
+            print('No such command. Try \'help\' to list all valid commands.');
+        }
+
+        cons.innerHTML += '<br>' + front;
+        inp.value = '';
+        var objDiv = document.getElementById('screen');
+        objDiv.scrollTop = objDiv.scrollHeight;
+
+        function openFile(file) {
+            if (fileList.includes(file)) {
+                print('————————————————————————————————————————————————');
+                if (file == 'file.txt') {
+                    print('This is a placeholder text file.');
+                } else if (file == 'count.exe') {
+                    for (let i = 0; i < 100; i++) {
+                        print(i);
+                    }
+
+                    print('Finished counting, exiting program...');
+                }
+                print('————————————————————————————————————————————————');
+            } else {
+                print('No such file. Please ensure that there are no additional characters in your command.');
+            }
         }
     }
 }
