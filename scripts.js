4a6d9b8b1ee9be26f1b0555dedf2bb7930d1cbd3
Nothing <marcus.low.bok.yat@gmail.com>
2022-07-11 21:21:41 +0800

    v1.1
    nothing lol

--- scripts.js
+++ scripts.js
@@ -4,11 +4,12 @@ const cons = document.getElementById('console');
 
 let cmdDict = {
     help: 'List of valid commands (this)',
+    cd: 'Change directory',
     ls: 'List of all existing files',
     open: 'Open an existing file'
 };
 
-let fileList = ['file.txt', 'count.exe'];
+let fileList = ['folder/', 'file.txt', 'count.exe', 'folder/test.txt'];
 
 function processInput() {
     if (inp.value != '') {
@@ -25,6 +26,8 @@ function processInput() {
             };
         } else if (cmd[0] == 'open') {
             openFile(cmd[1]);
+        } else if (cmd[0] == 'cd') {
+            changeDir(cmd[1]);
         } else {
             print('No such command. Try \'help\' to list all valid commands.');
         }
@@ -35,7 +38,7 @@ function processInput() {
         objDiv.scrollTop = objDiv.scrollHeight;
 
         function openFile(file) {
-            if (fileList.includes(file)) {
+            if (fileList.includes(file) && !file.includes('/')) {
                 print('————————————————————————————————————————————————');
                 if (file == 'file.txt') {
                     print('This is a placeholder text file.');
@@ -49,6 +52,19 @@ function processInput() {
                 print('————————————————————————————————————————————————');
             } else {
                 print('No such file. Please ensure that there are no additional characters in your command.');
+            }
+        }
+
+        function changeDir(d) {
+            var dir;
+            if (fileList.includes(d) || fileList.includes(d + '/')) {
+                print('PLACEHOLDER 101');
+                dir = 'd';
+            } else if (d == '..') {
+                print('PLACEHOLDER 102');
+                dir = '';
+            } else {
+                print('No such directory.');
             }
         }
     }
